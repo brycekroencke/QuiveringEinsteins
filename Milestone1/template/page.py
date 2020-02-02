edb8a114f225efca12c53dae6d8149964d58a33f
Mostafa K Herwees <44454154+Mkherwees@users.noreply.github.com>
2020-02-01 20:47:54 -0800

    Page insert.

--- Milestone1/template/page.py
+++ Milestone1/template/page.py
@@ -16,4 +16,16 @@ class Page:
 
     def write(self, value):
         self.num_records += 1
+        bytevalue = (value).to_bytes(8, byteorder='big')
+        i = self.num_records * 8
+
+        data[i + 0] = bytevalue[0]
+        data[i + 1] = bytevalue[1]
+        data[i + 2] = bytevalue[2]
+        data[i + 3] = bytevalue[3]
+        data[i + 4] = bytevalue[4]
+        data[i + 5] = bytevalue[5]
+        data[i + 6] = bytevalue[6]
+        data[i + 7] = bytevalue[7]
+
         pass
