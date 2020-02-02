c2f520938c5140864d9230587afa4ff3bc3579f5
Mostafa K Herwees <44454154+Mkherwees@users.noreply.github.com>
2020-02-01 22:20:49 -0800

    Read data with index.

--- Milestone1/template/page.py
+++ Milestone1/template/page.py
@@ -12,8 +12,6 @@ class Page:
     # def has_capacity(self):
     #     return self.capacity
 
-
-
     def write(self, value):
         self.num_records += 1
         bytevalue = (value).to_bytes(8, byteorder='big')
@@ -28,4 +26,16 @@ class Page:
         data[i + 6] = bytevalue[6]
         data[i + 7] = bytevalue[7]
 
-        pass
+    def read(self, index):
+        bytevalue = bytearray(8)
+
+        bytevalue[0] = data[index + 0]
+        bytevalue[1] = data[index + 1]
+        bytevalue[2] = data[index + 2]
+        bytevalue[3] = data[index + 3]
+        bytevalue[4] = data[index + 4]
+        bytevalue[5] = data[index + 5]
+        bytevalue[6] = data[index + 6]
+        bytevalue[7] = data[index + 7]
+
+        return int.from_bytes(bytevalue, byteorder='big')
