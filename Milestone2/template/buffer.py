dad54bbf5060b31d3dcc59f41bf27066749443a0
Draco <hdjchen@ucdavis.edu>
2020-02-23 16:16:16 -0800

    updating buffer

--- Milestone2/template/buffer.py
+++ Milestone2/template/buffer.py
@@ -3,6 +3,17 @@ class Buffer:
         buffer_size = 2
         book_range = 1
         tail_list_length = int(buffer_size/book_range)
-        
+
         self.base_book_list = [None]*buffer_size
         self.tail_book_list = [[]]*tail_list_length
+
+    # Return a list of tail book to be merged.
+    # This list will be the merge_queue in the merge function
+    # in Table class.
+    def full_tail_book_list(self):
+        merge_tail_books = []
+        for tail_book in tail_book_list:
+            # The tail_book_list store a list of pages.
+            if len(tail_book) > 2:
+                merge_tail_books.append(tail_book)
+        return merge_tail_books
