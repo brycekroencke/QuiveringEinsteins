a22c4bdb0cb45a8e429c89595b5e52708170eb50
DaneArcher <danearcher1@gmail.com>
2020-02-23 16:49:06 -0800

    removed record class

--- Milestone2/template/table.py
+++ Milestone2/template/table.py
@@ -11,12 +11,6 @@ RID_COLUMN = 1
 TIMESTAMP_COLUMN = 2
 SCHEMA_ENCODING_COLUMN = 3
 
-class Record:
-    def __init__(self, rid, key, columns):
-        self.rid = rid
-        self.key = key
-        self.columns = columns
-
 class Table:
     """
     :param name: string         #Table name
