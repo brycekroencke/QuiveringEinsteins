6e99befa73eebbbbd1bbc3fdef3fb8fa9d849092
Mostafa K Herwees <44454154+Mkherwees@users.noreply.github.com>
2020-02-28 18:38:01 -0800

    adsf

--- Milestone2/template/db.py
+++ Milestone2/template/db.py
@@ -29,7 +29,7 @@ class Database():
             # self.tables[idi].close = True
             # self.tables[idi].merge_thread.join()
             for idj, j in enumerate(i.buffer_pool.buffer):
-                print(j.bookindex)
+                if j != None:
                 self.tables[idi].dump_book_json(j)
         del self #Rip database
 
