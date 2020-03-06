ab83ae9d80519fe98cfa2601136562a7667559e9
Mostafa K Herwees <44454154+Mkherwees@users.noreply.github.com>
2020-03-05 16:16:25 -0800

    missed a file

---
+++ Milestone3/template/transaction.py
@@ -0,0 +1,33 @@
+from template.table import Table, Record
+from template.index import Index
+
+class Transaction:
+
+    """
+    # Creates a transaction object.
+    """
+    def __init__(self):
+        self.queries = []
+        pass
+
+    """
+    # Adds the given query to this transaction
+    # Example:
+    # q = Query(grades_table)
+    # t = Transaction()
+    # t.add_query(q.update, 0, *[None, 1, None, 2, None])
+    """
+    def add_query(self, query, *args):
+        self.queries.append((query, args))
+
+    # This MUST return 0 if transaction is sucessful, else it must return 0
+    def run(self):
+        for query, args in self.queries:
+            query(*args)
+        return 1
+
+    def abort(self):
+        pass
+
+    def commit(self):
+        pass
